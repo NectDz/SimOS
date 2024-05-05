d823dffd78e10366c534fdac354c1437b74ddff7
NectDz <54990613+NectDz@users.noreply.github.com>
2024-05-05 18:34:30 -0400

    Update Disk.h

--- Disk.h
+++ Disk.h
@@ -5,20 +5,15 @@
 #include <string>
 #include <deque>
 #include "PCB.h"
-
-struct FileReadRequest
-{
-    int  PID{0};
-    std::string fileName{""};
-};
+#include "SimOS.h"
 
 class Disk {
 private:
-    int diskNumber; // Unique identifier for the disk
-    std::deque<FileReadRequest> ioQueue; // Queue for managing I/O requests
+    int diskNumber;
+    std::deque<FileReadRequest> ioQueue; 
 
 public:
-    Disk(int num) : diskNumber(num) {} // Constructor to initialize the disk with a disk number
+    Disk(int num) : diskNumber(num) {} 
 
     void addRequest(const FileReadRequest& request) {
         ioQueue.push_back(request);
@@ -27,9 +22,7 @@ public:
     void processRequest() {
         if (!ioQueue.empty()) {
             FileReadRequest request = ioQueue.front();
-            // Simulate reading the file
             ioQueue.pop_front();
-            // Notify completion (this could be a callback or a signal to the process)
         }
     }
 
